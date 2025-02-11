9952b65c176f126100fbb4e612f4cb32c8868c38
Jack <jackn25@nycstudents.net>
2025-02-11 13:06:02 -0500

    finished l4

--- src/L4/ArrayListAlgorithms.java
+++ src/L4/ArrayListAlgorithms.java
@@ -1,30 +1,118 @@
 package L4;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArrayListAlgorithms {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>();
-        names.add("Jessica");
-        names.add("Emily");
-        names.add("Destiny");
-        names.add("Mateo");
-        names.add("Sofia");
-        names.add("Tim");
-        names.add("James");
+        ArrayList<Integer> intList7 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+        ArrayList<Integer> intList8 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+        ArrayList<Integer> intList9 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 6));
+        ArrayList<Integer> intList10 = new ArrayList<Integer>(Arrays.asList(1, 2, 7, 4, 6));
+        boolean identical = ArrayListAlgorithms.areIdentical(intList7, intList8);
+        System.out.println(identical);
+        identical = ArrayListAlgorithms.areIdentical(intList8, intList9);
+        System.out.println(identical);
+        identical = ArrayListAlgorithms.areIdentical(intList9, intList10);
+        System.out.println(identical);
+    }
 
-        // before:
-        System.out.println(names);
+    /** Returns true if any of the elements in stringList contain
+     *  target as a substring; false otherwise.
+     *
+     *  Does NOT mutate (modify) elements of stringList.
+     *  PRECONDITION: stringList.size() > 0
+     *
+     *  @param stringList  original arraylist of Strings
+     *  @return  true if target is found in any of the strings, false otherwise
+     */
+    public static boolean containsTarget(ArrayList<String> stringList, String target)
+    {
+        for (String string : stringList) {
+            if (string.contains(target)) return true;
+        }
 
-        // complete me:
-        for (int idx=0; idx < names.size(); idx++)
-        {
-            if (names.get(idx).length() == 5) {
-                names.remove(idx);
-                idx --;
+        return false;
+    }
+
+
+    /** Returns number of elements in intList that are less than the
+     *  average of all elements.
+     *
+     *  Does NOT mutate (modify) elements of intList.
+     *  PRECONDITION: intList.size() > 0
+     *
+     *  @param intList  original arraylist of Integers
+     *  @return  the number of ints in intList that are less than the average
+     */
+    public static int belowAverage(ArrayList<Integer> intList)
+    {
+        double avg = 0;
+        int cnt = 0;
+
+        for (int i : intList) {
+            avg += i;
+        }
+
+        avg /= intList.size();
+
+        for (int i : intList) {
+            if (i < avg) {
+                cnt ++;
             }
         }
 
-        System.out.println(names);
+        return cnt;
+    }
+
+    /** Replaces all words in wordList that end in "s" with the all-uppercase
+     *  version of the word.  For example, "apples" should be replaced with "APPLES".
+     *  Assume all letters of all words in wordList are lowercase initially (no need
+     *  to worry about checking for case or converting first to lowercase)
+     *
+     *  DOES mutate (modify) elements of wordList.
+     *  PRECONDITION: wordList.size() > 0
+     *
+     *  @param wordList  original arraylist of words
+     */
+    public static void replaceWithCaps(ArrayList<String> wordList)
+    {
+        for (int i = 0; i < wordList.size(); i++) {
+            String word = wordList.get(i);
+
+            if (word.endsWith("s")) {
+                wordList.set(i, word.toUpperCase());
+            }
+        }
+    }
+
+    /** Returns the index at which the minimum value of all integers in
+     *  intList appears; if the minimum value appears more than once in
+     *  the arraylist, return the index of the first occurrence
+     *
+     *  Does NOT mutate (modify) elements in intList.
+     *  PRECONDITION: intList.size() > 0
+     *
+     *  @param intList  original arraylist of Integers
+     *  @return  the index at which the minimum value occurs
+     */
+    public static int indexOfMinimum(ArrayList<Integer> intList)
+    {
+        int min = Integer.MAX_VALUE;
+        int idx = 0;
+
+        for (int i = 0; i < intList.size(); i++) {
+            if (intList.get(i) < min) {
+                min = intList.get(i);
+                idx = i;
+            }
+        }
+
+        return idx;
+    }
+
+    public static boolean areIdentical(ArrayList<Integer> numList1, ArrayList<Integer> numList2)
+    {
+        return numList1.equals(numList2);
     }
 }
