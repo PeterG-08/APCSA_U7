bfff7cc12312c24b61ab2da00afe23eeab0338da
Jack <jackn25@nycstudents.net>
2025-02-12 13:07:55 -0500

    finished l5

--- src/L5/CarRepair.java
+++ src/L5/CarRepair.java
@@ -1,4 +1,28 @@
 package L5;
 
-public class CarRepair {
+public class CarRepair
+{
+    private int mechanicNum;
+    private int bayNum;
+
+    public CarRepair(int m, int b)
+    {
+        mechanicNum = m;
+        bayNum = b;
+    }
+
+    public int getMechanicNum()
+    {
+        return mechanicNum;
+    }
+
+    public int getBayNum()
+    {
+        return bayNum;
+    }
+
+    public String toString()
+    {
+        return "mechanic num: " + mechanicNum + ", bay num: " + bayNum;
+    }
 }
