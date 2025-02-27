0f57a4d53c0a51944001641d4645ef41fd1d43f7
Jack <jackn25@nycstudents.net>
2025-02-27 13:10:40 -0500

    finished l7

--- src/L7/Sort.java
+++ src/L7/Sort.java
@@ -53,11 +53,46 @@ public class Sort
     // Part C.  Sorting a 1000-word list!
     public static void selectionSortWordList(ArrayList<String> words)
     {
-        /* TO BE IMPLEMENTED IN PART C */
+        int counter = 0;
+
+        for (int j = 0; j < words.size() - 1; j++)
+        {
+            int minIndex = j;
+            for (int k = j + 1; k < words.size(); k++)
+            {
+                counter ++;
+
+                if (words.get(k).compareTo(words.get(minIndex)) < 0)
+                {
+                    minIndex = k;
+                }
+            }
+            String temp = words.get(j);
+            words.set(j, words.get(minIndex));
+            words.set(minIndex, temp);
+        }
+
+        System.out.println("SELECTION COUNTER: " + counter);
     }
 
     public static void insertionSortWordList(ArrayList<String> words)
     {
-        /* TO BE IMPLEMENTED IN PART C */
+        int counter = 0;
+
+        for (int j = 1; j < words.size(); j++)
+        {
+            String temp = words.get(j);
+            int possibleIndex = j;
+            while (possibleIndex > 0 && temp.compareTo(words.get(possibleIndex - 1)) < 0)
+            {
+                words.set(possibleIndex, words.get(possibleIndex - 1));
+                possibleIndex--;
+
+                counter ++;
+            }
+            words.set(possibleIndex, temp);
+        }
+
+        System.out.println("INSERTION COUNTER: " + counter);
     }
 }
